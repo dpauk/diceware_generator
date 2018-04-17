e9959f97319e12bf732c1307142e2bf4274cd6eb
David Albone <davd.p.albone@gmail.com>
2018-04-17 21:27:20 +0100

    Added the ability to add an argument related to the number of word in phrase - no tests yet though

--- src/main/java/com/davidalbone/diceware/DicewareMapping.java
+++ src/main/java/com/davidalbone/diceware/DicewareMapping.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 
 public class DicewareMapping {
     private HashMap<String, String> numberWordMapping = new HashMap<>();
--- src/main/java/com/davidalbone/diceware/Main.java
+++ src/main/java/com/davidalbone/diceware/Main.java
@@ -1,11 +1,30 @@
 package com.davidalbone.diceware;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
-    public static void main(String[] args) {
-        DicewarePasscodeGenerator dpg = new DicewarePasscodeGenerator();
-        String generatedPasscode = dpg.getLastPasscodeGenerated();
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-        System.out.println(String.format("Password is: %s%n", generatedPasscode)); // NOSONAR
+    public static void main(String[] args) {
+        DicewarePasscodeGenerator dpg = null;
+
+        if (args.length == 0) {
+            dpg = new DicewarePasscodeGenerator();
+        } else {
+            try {
+                dpg = new DicewarePasscodeGenerator(Integer.parseInt(args[0]));
+            } catch (NumberFormatException nfe) {
+                logger.log(Level.SEVERE, "Invalid number passed as argument");
+                System.exit(1);
+            }
+        }
+
+        if (dpg != null) {
+            String generatedPasscode = dpg.getLastPasscodeGenerated();
+
+            System.out.println(String.format("Password is: %s%n", generatedPasscode)); // NOSONAR
+        }
     }
 }
